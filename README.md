67195ff9020178a672e3ab8a0ea91b2faddacdd3
cynthiamaia <cynthiamaia45@gmail.com>
2022-06-15 20:27:04 -0300

    arquivos do benchmark

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# benchmark
+# AutoML Benchmark
+The AutoML Benchmark provides an overview and comparison of open-source AutoML systems
+# Steps
+## Installation Linux Ubuntu
+### Pre-requisites
+To run the benchmark, you will need:
+- Python 3.7+
+- PIP3
+
+###  Installation
+- Download Folder: SystemMetaLearning
+
+```sh
+sudo apt-get update
+sudo apt install python3-venv
+sudo apt-get install python3-virtualenv
+```
+## Installation Windows
+### Pre-requisites
+To run the benchmark, you will need:
+- [Python](https://www.python.org/downloads/release/python-375/) - Python
+```sh
+cd SystemMetaLearning
+```
+Create a virtual environment Linux Ubuntu:
+```sh
+python3 -m venv ./venv
+source venv/bin/activate
+```
+Create a virtual environment Windows:
+```sh
+python3 -m venv ./venv
+source venv\Scripts\activate
+```
+Then pip install the dependencies:
+
+```sh
+python -m pip install -r requirements.txt
+```
+
+### Quickstart
+
+```sh
+python3 runbenchmark.py autoweka exampletest test
+```
+
+
---
+++ frameworks/AutoGluon/setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+HERE=$(dirname "$0")
+VERSION=${1:-"stable"}
+REPO=${2:-"https://github.com/awslabs/autogluon.git"}
+PKG=${3:-"autogluon"}
+if [[ "$VERSION" == "latest" ]]; then
+    VERSION="master"
+fi
+
+# creating local venv
+. ${HERE}/../shared/setup.sh ${HERE} true
+#if [[ -x "$(command -v apt-get)" ]]; then
+#    SUDO apt-get install -y libomp-dev
+if [[ -x "$(command -v brew)" ]]; then
+    brew install libomp
+fi
+
+PIP install --upgrade pip
+PIP install --upgrade setuptools wheel
+PIP install "mxnet<2.0.0"
+PIP install "scikit-learn-intelex<2021.3"
+
+if [[ "$VERSION" == "stable" ]]; then
+    PIP install --no-cache-dir -U ${PKG}
+elif [[ "$VERSION" =~ ^[0-9] ]]; then
+    PIP install --no-cache-dir -U ${PKG}==${VERSION}
+else
+    TARGET_DIR="${HERE}/lib/${PKG}"
+    rm -Rf ${TARGET_DIR}
+    git clone --depth 1 --single-branch --branch ${VERSION} --recurse-submodules ${REPO} ${TARGET_DIR}
+    cd ${TARGET_DIR}
+    PIP install -e core/
+    PIP install -e features/
+    PIP install -e tabular/[all]
+    PIP install -e mxnet/
+    PIP install -e extra/
+    PIP install -e text/
+    PIP install -e vision/
+    PIP install -e autogluon/
+fi
+
+PY -c "from autogluon.tabular.version import __version__; print(__version__)" >> "${HERE}/.setup/installed"
